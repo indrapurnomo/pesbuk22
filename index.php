c5b79e26f01e0602d753d0cce51be04c64d87ddc
Indra Purnomo <indra.purnomo@mail.ugm.ac.id>
2020-11-11 11:50:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pesbuk-Big-Pro</title>
+    <title>Pesbuk-Big-Pro-06</title>
 
      <link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
